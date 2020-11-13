c2d199abaa70016cac764237218bfc8bee14fd59
jwtracy <58991818+jwtracy@users.noreply.github.com>
2020-11-13 12:22:01 -0800

    Revert "Consolidate dep updates"

--- lib/url-helper.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="node" />
-import { IGitSourceSettings } from './git-source-settings';
-import { URL } from 'url';
-export declare function getApiUrl(): string;
-export declare function getFetchUrl(settings: IGitSourceSettings): string;
-export declare function getServerUrl(): URL;
